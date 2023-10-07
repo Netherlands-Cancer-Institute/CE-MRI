10c57081f39906c4e62c8551df013ae3f502f7ab
Tianyu <t.zhang@nki.nl>
2023-10-07 07:14:48 +0200

    Create attention_module.py

---
+++ scrs/attention_module.py
@@ -0,0 +1,25 @@
+import tensorflow.keras.layers as KL
+
+def parameter_attention(input_xs, reduction_ratio=0.5):
+
+    channel = int(input_xs.shape[channel_axis])
+    maxpool_channel = KL.GlobalMaxPooling2D()(input_xs)
+    maxpool_channel = KL.Reshape((1, 1, channel))(maxpool_channel)
+    avgpool_channel = KL.GlobalAvgPool2D()(input_xs)
+    avgpool_channel = KL.Reshape((1, 1, channel))(avgpool_channel)
+    Dense_One = KL.Dense(units=int(channel * reduction_ratio), activation='relu', kernel_initializer='he_normal',
+                         use_bias=True, bias_initializer='zeros')
+    Dense_Two = KL.Dense(units=int(channel), activation='relu', kernel_initializer='he_normal', use_bias=True,
+                         bias_initializer='zeros')
+
+    mlp_1_max = Dense_One(maxpool_channel)
+    mlp_2_max = Dense_Two(mlp_1_max)
+    mlp_2_max = KL.Reshape(target_shape=(1, 1, int(channel)))(mlp_2_max)
+
+    mlp_1_avg = Dense_One(avgpool_channel)
+    mlp_2_avg = Dense_Two(mlp_1_avg)
+    mlp_2_avg = KL.Reshape(target_shape=(1, 1, int(channel)))(mlp_2_avg)
+    channel_attention_feature = KL.Add()([mlp_2_max, mlp_2_avg])
+    channel_attention_feature = KL.Activation('sigmoid')(channel_attention_feature)
+    refined_feature = KL.Multiply()([channel_attention_feature, input_xs])
+    return KL.Add()([refined_feature, input_xs])
